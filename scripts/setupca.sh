c8ebabf64f6ffd3b77b04b30638a54faf9c5b6fc
HQCATALYST\imran.qureshi <imran.qureshi@healthcatalyst.com>
2017-09-29 16:41:16 -0700

    change CA name to HealthCatalystRealtimeCA
    Signed-off-by: HQCATALYST\imran.qureshi <imran.qureshi@healthcatalyst.com>

--- scripts/setupca.sh
+++ scripts/setupca.sh
@@ -5,5 +5,5 @@ mkdir certs private
 chmod 700 private
 echo 01 > serial
 touch index.txt
-openssl req -x509 -config openssl.cnf -newkey rsa:2048 -days 365 -out cacert.pem -outform PEM -subj /CN=MyTestCA/ -nodes
+openssl req -x509 -config openssl.cnf -newkey rsa:2048 -days 365 -out cacert.pem -outform PEM -subj /CN=HealthCatalystRealtimeCA/ -nodes
 openssl x509 -in cacert.pem -out cacert.cer -outform DER
