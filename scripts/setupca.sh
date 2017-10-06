78171ee392b28a31fbfb3138658ea98c2a611ecb
HQCATALYST\imran.qureshi <imran.qureshi@healthcatalyst.com>
2017-10-06 16:45:57 -0700

    change the CA name
    Signed-off-by: HQCATALYST\imran.qureshi <imran.qureshi@healthcatalyst.com>

--- scripts/setupca.sh
+++ scripts/setupca.sh
@@ -9,7 +9,7 @@ cd /home/testca
 
 # Create a self-signed certificate that will serve a certificate authority (CA).
 # The private key is located under "private".
-openssl req -x509 -config openssl.cnf -newkey rsa:2048 -days 365 -out cacert.pem -outform PEM -subj /CN=MyTestCA/ -nodes
+openssl req -x509 -config openssl.cnf -newkey rsa:2048 -days 365 -out cacert.pem -outform PEM -subj /CN=FabricRabbitMqCA/ -nodes
 
 # Encode our certificate with DER.
 openssl x509 -in cacert.pem -out cacert.cer -outform DER
