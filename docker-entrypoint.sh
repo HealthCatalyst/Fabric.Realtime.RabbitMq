2eee42aa478d9921cc53235018d23250f03dffd9
HQCATALYST\imran.qureshi <imran.qureshi@healthcatalyst.com>
2017-10-06 19:09:38 -0700

    add more logging
    Signed-off-by: HQCATALYST\imran.qureshi <imran.qureshi@healthcatalyst.com>

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,7 +2,14 @@
 
 set -eu
 
-if [ ! -f /opt/healthcatalyst/client/cert.pem ]
+echo "running docker-entrypoint.sh"
+
+echo "contents of /opt/healthcatalyst/client/"
+echo "-------"
+ls /opt/healthcatalyst/client/
+echo "-------"
+
+if [ ! -f "/opt/healthcatalyst/client/cert.pem" ]
 then
 	echo "no certificates found so regenerating them"
 	/bin/bash /opt/healthcatalyst/setupca.sh \
