8138659b04f3b2afbc70aa10f17b108ead4e9da6
HQCATALYST\imran.qureshi <imran.qureshi@healthcatalyst.com>
2017-10-06 19:55:18 -0700

    removed {ssl_cert_login_from, common_name} from config since that was causing rabbitmq to error
    Signed-off-by: HQCATALYST\imran.qureshi <imran.qureshi@healthcatalyst.com>

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -11,7 +11,6 @@ echo "-------"
 
 if [ ! -f "/opt/healthcatalyst/client/cert.pem" ]
 then
-
 	echo "no certificates found so regenerating them"
 	/bin/bash /opt/healthcatalyst/setupca.sh \
 		&& /bin/bash /opt/healthcatalyst/generateservercert.sh Imran \
