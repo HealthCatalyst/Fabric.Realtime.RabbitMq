dce17f9973161527f4c8da05369415b2b1f8169d
HQCATALYST\imran.qureshi <imran.qureshi@healthcatalyst.com>
2017-10-08 19:44:45 -0700

    enable ssl plugin and create user in docker-entrypoint.sh
    Signed-off-by: HQCATALYST\imran.qureshi <imran.qureshi@healthcatalyst.com>

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -14,15 +14,20 @@ then
 	echo "no certificates found so regenerating them"
 	/bin/bash /opt/healthcatalyst/setupca.sh \
 		&& /bin/bash /opt/healthcatalyst/generateservercert.sh Imran \
-		&& /etc/init.d/rabbitmq-server restart
-		
-	echo "enabling ssl auth plugin"
-	rabbitmq-plugins enable rabbitmq_auth_mechanism_ssl
-
-	/bin/bash /opt/healthcatalyst/generateclientcert.sh Imran \
+		&& /etc/init.d/rabbitmq-server restart \
+		&& /bin/bash /opt/healthcatalyst/generateclientcert.sh Imran \
 		&& /etc/init.d/rabbitmq-server stop
 else
 	echo "certificates already exist so we're not regenerating them"
 fi
+
+echo "enabling ssl auth plugin" \
+	&& /etc/init.d/rabbitmq-server restart \
+	&& rabbitmq-plugins enable rabbitmq_auth_mechanism_ssl \
+	&& echo "creating fabricrabbitmquser user" \
+	&& rabbitmqctl add_user fabricrabbitmquser test \
+	&& rabbitmqctl set_user_tags fabricrabbitmquser administrator \
+	&& rabbitmqctl set_permissions -p / fabricrabbitmquser ".*" ".*" ".*" \
+	&& /etc/init.d/rabbitmq-server stop
 
 exec rabbitmq-server
